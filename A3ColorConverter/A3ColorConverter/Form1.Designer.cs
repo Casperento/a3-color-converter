a47a87f12e060727ba6c0fad7c7624a0eab5a3ac
Casperento <44746868+Casperento@users.noreply.github.com>
2020-05-26 22:57:28 -0300

    New color picker and UI rework...

--- A3ColorConverter/A3ColorConverter/Form1.Designer.cs
+++ A3ColorConverter/A3ColorConverter/Form1.Designer.cs
@@ -29,101 +29,226 @@
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
-            this.txtBoxHexValue = new System.Windows.Forms.TextBox();
-            this.txtBoxResultValue = new System.Windows.Forms.TextBox();
+            this.txtBoxHexTit = new System.Windows.Forms.TextBox();
+            this.txtBoxAcolorTit = new System.Windows.Forms.TextBox();
             this.lblTypeHex = new System.Windows.Forms.Label();
             this.lblResult = new System.Windows.Forms.Label();
-            this.btnConvert = new System.Windows.Forms.Button();
-            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnHtoA1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnPicker1 = new System.Windows.Forms.Button();
+            this.btnAtoH1 = new System.Windows.Forms.Button();
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.lblTit = new System.Windows.Forms.Label();
+            this.btnPicker2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxAcolorBg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxHexBg = new System.Windows.Forms.TextBox();
+            this.btnAtoH2 = new System.Windows.Forms.Button();
+            this.btnHtoA2 = new System.Windows.Forms.Button();
+            this.colorPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
-            this.lblTitle.Location = new System.Drawing.Point(41, 19);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitle.Location = new System.Drawing.Point(44, 28);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(206, 13);
+            this.lblTitle.Size = new System.Drawing.Size(441, 20);
             this.lblTitle.TabIndex = 1;
-            this.lblTitle.Text = "Convert HEX colors to Arma 3 RGB format";
+            this.lblTitle.Text = "Type some HEX value to convert to Arma\'s #RGB or #ARGB:";
             // 
-            // txtBoxHexValue
+            // txtBoxHexTit
             // 
-            this.txtBoxHexValue.Location = new System.Drawing.Point(31, 64);
-            this.txtBoxHexValue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtBoxHexValue.Name = "txtBoxHexValue";
-            this.txtBoxHexValue.Size = new System.Drawing.Size(239, 20);
-            this.txtBoxHexValue.TabIndex = 2;
+            this.txtBoxHexTit.BackColor = System.Drawing.SystemColors.Window;
+            this.txtBoxHexTit.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBoxHexTit.ForeColor = System.Drawing.Color.Black;
+            this.txtBoxHexTit.Location = new System.Drawing.Point(46, 98);
+            this.txtBoxHexTit.Name = "txtBoxHexTit";
+            this.txtBoxHexTit.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtBoxHexTit.Size = new System.Drawing.Size(196, 26);
+            this.txtBoxHexTit.TabIndex = 2;
+            this.txtBoxHexTit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // txtBoxResultValue
+            // txtBoxAcolorTit
             // 
-            this.txtBoxResultValue.Location = new System.Drawing.Point(31, 120);
-            this.txtBoxResultValue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtBoxResultValue.Name = "txtBoxResultValue";
-            this.txtBoxResultValue.ReadOnly = true;
-            this.txtBoxResultValue.Size = new System.Drawing.Size(239, 20);
-            this.txtBoxResultValue.TabIndex = 3;
+            this.txtBoxAcolorTit.Location = new System.Drawing.Point(46, 253);
+            this.txtBoxAcolorTit.Name = "txtBoxAcolorTit";
+            this.txtBoxAcolorTit.Size = new System.Drawing.Size(196, 26);
+            this.txtBoxAcolorTit.TabIndex = 3;
+            this.txtBoxAcolorTit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lblTypeHex
             // 
             this.lblTypeHex.AutoSize = true;
-            this.lblTypeHex.Location = new System.Drawing.Point(29, 49);
-            this.lblTypeHex.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTypeHex.Location = new System.Drawing.Point(44, 75);
             this.lblTypeHex.Name = "lblTypeHex";
-            this.lblTypeHex.Size = new System.Drawing.Size(97, 13);
+            this.lblTypeHex.Size = new System.Drawing.Size(122, 20);
             this.lblTypeHex.TabIndex = 4;
-            this.lblTypeHex.Text = "Type a HEX value:";
+            this.lblTypeHex.Text = "Text HEX Color:";
             // 
             // lblResult
             // 
             this.lblResult.AutoSize = true;
-            this.lblResult.Location = new System.Drawing.Point(29, 105);
-            this.lblResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResult.Location = new System.Drawing.Point(44, 230);
             this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(40, 13);
+            this.lblResult.Size = new System.Drawing.Size(84, 20);
             this.lblResult.TabIndex = 5;
-            this.lblResult.Text = "Result:";
+            this.lblResult.Text = "Text Color:";
             // 
-            // btnConvert
+            // btnHtoA1
             // 
-            this.btnConvert.Location = new System.Drawing.Point(63, 159);
-            this.btnConvert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(67, 21);
-            this.btnConvert.TabIndex = 6;
-            this.btnConvert.Text = "Convert";
-            this.btnConvert.UseVisualStyleBackColor = true;
-            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            this.btnHtoA1.Location = new System.Drawing.Point(78, 151);
+            this.btnHtoA1.Name = "btnHtoA1";
+            this.btnHtoA1.Size = new System.Drawing.Size(31, 49);
+            this.btnHtoA1.TabIndex = 6;
+            this.btnHtoA1.Text = "V";
+            this.btnHtoA1.UseVisualStyleBackColor = true;
+            this.btnHtoA1.Click += new System.EventHandler(this.btnConvert_Click);
             // 
-            // btnCopy
+            // colorDialog1
             // 
-            this.btnCopy.Location = new System.Drawing.Point(167, 159);
-            this.btnCopy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnCopy.Name = "btnCopy";
-            this.btnCopy.Size = new System.Drawing.Size(67, 21);
-            this.btnCopy.TabIndex = 7;
-            this.btnCopy.Text = "Copy";
-            this.btnCopy.UseVisualStyleBackColor = true;
-            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            this.colorDialog1.AnyColor = true;
+            this.colorDialog1.FullOpen = true;
+            // 
+            // btnPicker1
+            // 
+            this.btnPicker1.Location = new System.Drawing.Point(46, 294);
+            this.btnPicker1.Name = "btnPicker1";
+            this.btnPicker1.Size = new System.Drawing.Size(196, 32);
+            this.btnPicker1.TabIndex = 8;
+            this.btnPicker1.Text = "Picker";
+            this.btnPicker1.UseVisualStyleBackColor = true;
+            this.btnPicker1.Click += new System.EventHandler(this.btnPicker_Click);
+            // 
+            // btnAtoH1
+            // 
+            this.btnAtoH1.Enabled = false;
+            this.btnAtoH1.Location = new System.Drawing.Point(115, 151);
+            this.btnAtoH1.Name = "btnAtoH1";
+            this.btnAtoH1.Size = new System.Drawing.Size(31, 50);
+            this.btnAtoH1.TabIndex = 10;
+            this.btnAtoH1.Text = "Ʌ";
+            this.btnAtoH1.UseVisualStyleBackColor = true;
+            this.btnAtoH1.Click += new System.EventHandler(this.btnAtoH1_Click);
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.colorPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorPanel.Controls.Add(this.lblTit);
+            this.colorPanel.Location = new System.Drawing.Point(162, 151);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(206, 49);
+            this.colorPanel.TabIndex = 11;
+            // 
+            // lblTit
+            // 
+            this.lblTit.AutoSize = true;
+            this.lblTit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTit.Location = new System.Drawing.Point(38, 9);
+            this.lblTit.Name = "lblTit";
+            this.lblTit.Size = new System.Drawing.Size(130, 29);
+            this.lblTit.TabIndex = 12;
+            this.lblTit.Text = "Some Text";
+            // 
+            // btnPicker2
+            // 
+            this.btnPicker2.Location = new System.Drawing.Point(276, 294);
+            this.btnPicker2.Name = "btnPicker2";
+            this.btnPicker2.Size = new System.Drawing.Size(196, 32);
+            this.btnPicker2.TabIndex = 14;
+            this.btnPicker2.Text = "Picker";
+            this.btnPicker2.UseVisualStyleBackColor = true;
+            this.btnPicker2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Background Color:";
+            // 
+            // txtBoxAcolorBg
+            // 
+            this.txtBoxAcolorBg.Location = new System.Drawing.Point(276, 253);
+            this.txtBoxAcolorBg.Name = "txtBoxAcolorBg";
+            this.txtBoxAcolorBg.Size = new System.Drawing.Size(196, 26);
+            this.txtBoxAcolorBg.TabIndex = 12;
+            this.txtBoxAcolorBg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Background HEX Color:";
+            // 
+            // txtBoxHexBg
+            // 
+            this.txtBoxHexBg.BackColor = System.Drawing.SystemColors.Window;
+            this.txtBoxHexBg.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtBoxHexBg.ForeColor = System.Drawing.Color.Black;
+            this.txtBoxHexBg.Location = new System.Drawing.Point(276, 98);
+            this.txtBoxHexBg.Name = "txtBoxHexBg";
+            this.txtBoxHexBg.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtBoxHexBg.Size = new System.Drawing.Size(196, 26);
+            this.txtBoxHexBg.TabIndex = 15;
+            this.txtBoxHexBg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnAtoH2
+            // 
+            this.btnAtoH2.Enabled = false;
+            this.btnAtoH2.Location = new System.Drawing.Point(420, 151);
+            this.btnAtoH2.Name = "btnAtoH2";
+            this.btnAtoH2.Size = new System.Drawing.Size(31, 49);
+            this.btnAtoH2.TabIndex = 18;
+            this.btnAtoH2.Text = "Ʌ";
+            this.btnAtoH2.UseVisualStyleBackColor = true;
+            this.btnAtoH2.Click += new System.EventHandler(this.btnAtoH2_Click);
+            // 
+            // btnHtoA2
+            // 
+            this.btnHtoA2.Location = new System.Drawing.Point(383, 151);
+            this.btnHtoA2.Name = "btnHtoA2";
+            this.btnHtoA2.Size = new System.Drawing.Size(31, 49);
+            this.btnHtoA2.TabIndex = 17;
+            this.btnHtoA2.Text = "V";
+            this.btnHtoA2.UseVisualStyleBackColor = true;
+            this.btnHtoA2.Click += new System.EventHandler(this.btnHtoA2_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(305, 197);
-            this.Controls.Add(this.btnCopy);
-            this.Controls.Add(this.btnConvert);
+            this.ClientSize = new System.Drawing.Size(526, 354);
+            this.Controls.Add(this.btnAtoH2);
+            this.Controls.Add(this.btnHtoA2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxHexBg);
+            this.Controls.Add(this.btnPicker2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxAcolorBg);
+            this.Controls.Add(this.colorPanel);
+            this.Controls.Add(this.btnAtoH1);
+            this.Controls.Add(this.btnPicker1);
+            this.Controls.Add(this.btnHtoA1);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.lblTypeHex);
-            this.Controls.Add(this.txtBoxResultValue);
-            this.Controls.Add(this.txtBoxHexValue);
+            this.Controls.Add(this.txtBoxAcolorTit);
+            this.Controls.Add(this.txtBoxHexTit);
             this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Arma 3 Color Converter";
+            this.Text = "ASCC v1.0.0 by Casperento";
+            this.colorPanel.ResumeLayout(false);
+            this.colorPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,12 +257,23 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.TextBox txtBoxHexValue;
-        private System.Windows.Forms.TextBox txtBoxResultValue;
+        private System.Windows.Forms.TextBox txtBoxHexTit;
+        private System.Windows.Forms.TextBox txtBoxAcolorTit;
         private System.Windows.Forms.Label lblTypeHex;
         private System.Windows.Forms.Label lblResult;
-        private System.Windows.Forms.Button btnConvert;
-        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnHtoA1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnPicker1;
+        private System.Windows.Forms.Button btnAtoH1;
+        private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.Label lblTit;
+        private System.Windows.Forms.Button btnPicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxAcolorBg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxHexBg;
+        private System.Windows.Forms.Button btnAtoH2;
+        private System.Windows.Forms.Button btnHtoA2;
     }
 }
 
--- A3ColorConverter/A3ColorConverter/Form1.cs
+++ A3ColorConverter/A3ColorConverter/Form1.cs
@@ -19,24 +19,157 @@ namespace A3ColorConverter
 
         private void btnConvert_Click(object sender, EventArgs e)
         {
-            String convertedValue;
-            String hexValue = txtBoxHexValue.Text;
+            if (txtBoxHexTit.Text == "") {
+                MessageBox.Show("You need to type something to convert...");
+            } else if (!txtBoxHexTit.Text.Contains('#') || txtBoxHexTit.Text.Length < 7)
+            {
+                MessageBox.Show("Invalid Color...");
+            } else {
+                String hexValue = txtBoxHexTit.Text;
+                decimal alpha, red, green, blue;
 
-            decimal red = int.Parse(hexValue.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
-            decimal green = int.Parse(hexValue.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
-            decimal blue = int.Parse(hexValue.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+                if (hexValue.Length < 8)
+                {
+                    alpha = 255;
+                    red = int.Parse(hexValue.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+                    green = int.Parse(hexValue.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+                    blue = int.Parse(hexValue.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+                } else
+                {
+                    alpha = int.Parse(hexValue.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+                    red = int.Parse(hexValue.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+                    green = int.Parse(hexValue.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+                    blue = int.Parse(hexValue.Substring(7, 2), System.Globalization.NumberStyles.HexNumber);
+                }
 
-            decimal r = Math.Round((red / 255) * 1000) / 1000;
-            decimal g = Math.Round((green / 255) * 1000) / 1000;
-            decimal b = Math.Round((blue / 255) * 1000) / 1000;
+                decimal a = Math.Round((alpha / 255) * 1000) / 1000;
+                decimal r = Math.Round((red / 255) * 1000) / 1000;
+                decimal g = Math.Round((green / 255) * 1000) / 1000;
+                decimal b = Math.Round((blue / 255) * 1000) / 1000;
 
-            convertedValue = String.Format("{0}, {1}, {2}, 1", r.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture), g.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture), b.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture));
-            txtBoxResultValue.Text = "{" + convertedValue + "}";
+                txtBoxAcolorTit.Text = "{" + String.Format("{0}, {1}, {2}, {3}",
+                    r.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    g.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    b.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    a.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture)
+                ) + "}";
+
+
+                lblTit.ForeColor = ColorTranslator.FromHtml(hexValue);
+            }
         }
 
-        private void btnCopy_Click(object sender, EventArgs e)
+        private void btnPicker_Click(object sender, EventArgs e)
         {
-            Clipboard.SetText(txtBoxResultValue.Text);
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.AnyColor = false;
+            colorDialog.FullOpen = true;
+            colorDialog.AllowFullOpen = true;
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                lblTit.ForeColor = colorDialog.Color;
+
+                var argbArr = BitConverter.GetBytes(colorDialog.Color.ToArgb()).Reverse().ToArray();
+
+                decimal a = Math.Round(( (decimal) argbArr[0] / 255) * 1000) / 1000;
+                decimal r = Math.Round(( (decimal) argbArr[1] / 255) * 1000) / 1000;
+                decimal g = Math.Round(( (decimal)argbArr[2] / 255) * 1000) / 1000;
+                decimal b = Math.Round(( (decimal)argbArr[3] / 255) * 1000) / 1000;
+
+                txtBoxAcolorTit.Text = "{" + String.Format("{0}, {1}, {2}, {3}",
+                    r.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    g.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    b.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    a.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture)
+                ) + "}";
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.AnyColor = false;
+            colorDialog.FullOpen = true;
+            colorDialog.AllowFullOpen = true;
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                colorPanel.BackColor = colorDialog.Color;
+
+                var argbArr = BitConverter.GetBytes(colorDialog.Color.ToArgb()).Reverse().ToArray();
+
+                decimal a = Math.Round(((decimal)argbArr[0] / 255) * 1000) / 1000;
+                decimal r = Math.Round(((decimal)argbArr[1] / 255) * 1000) / 1000;
+                decimal g = Math.Round(((decimal)argbArr[2] / 255) * 1000) / 1000;
+                decimal b = Math.Round(((decimal)argbArr[3] / 255) * 1000) / 1000;
+
+                txtBoxAcolorBg.Text = "{" + String.Format("{0}, {1}, {2}, {3}",
+                    r.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    g.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    b.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    a.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture)
+                ) + "}";
+
+            }
+        }
+
+        private void btnHtoA2_Click(object sender, EventArgs e)
+        {
+            if (txtBoxHexBg.Text == "")
+            {
+                MessageBox.Show("You need to type something to convert...");
+            }
+            else if (!txtBoxHexBg.Text.Contains('#') || txtBoxHexBg.Text.Length < 7)
+            {
+                MessageBox.Show("Invalid Color...");
+            }
+            else
+            {
+                String hexValue = txtBoxHexBg.Text;
+                decimal alpha, red, green, blue;
+
+                if (hexValue.Length < 8)
+                {
+                    alpha = 255;
+                    red = int.Parse(hexValue.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+                    green = int.Parse(hexValue.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+                    blue = int.Parse(hexValue.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+                }
+                else
+                {
+                    alpha = int.Parse(hexValue.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+                    red = int.Parse(hexValue.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+                    green = int.Parse(hexValue.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+                    blue = int.Parse(hexValue.Substring(7, 2), System.Globalization.NumberStyles.HexNumber);
+                }
+
+                decimal a = Math.Round((alpha / 255) * 1000) / 1000;
+                decimal r = Math.Round((red / 255) * 1000) / 1000;
+                decimal g = Math.Round((green / 255) * 1000) / 1000;
+                decimal b = Math.Round((blue / 255) * 1000) / 1000;
+
+                txtBoxAcolorBg.Text = "{" + String.Format("{0}, {1}, {2}, {3}",
+                    r.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    g.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    b.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture),
+                    a.ToString("0.000", System.Globalization.CultureInfo.InvariantCulture)
+                ) + "}";
+
+
+                colorPanel.BackColor = ColorTranslator.FromHtml(hexValue);
+            }
+        }
+
+        private void btnAtoH1_Click(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        private void btnAtoH2_Click(object sender, EventArgs e)
+        {
+            // TODO
         }
     }
 }
